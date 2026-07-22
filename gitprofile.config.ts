ab2b4f6a81b97a3320df0e0fcaf612be839a6cdd
Jack Brett <jdotbrett@gmail.com>
2026-07-22 09:13:33 +0100

    update (default theme to 'dim')

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -253,7 +253,7 @@ const CONFIG = {
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: { id: '', snippetVersion: 6 },
   themeConfig: {
-    defaultTheme: 'procyon',
+    defaultTheme: 'dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
